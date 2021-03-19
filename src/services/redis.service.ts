0e6d3d9b2976d359cf18e9ab5cb9fc548f39adb2
Dmitrii Korotkin <d.korotkin@corp.vk.com>
2021-03-19 06:26:40 +0300

    fix

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,7 +1,5 @@
-import { createClient } from 'async-redis';
+import { createClient, RedisClient } from 'async-redis';
 import Timeout = NodeJS.Timeout;
-
-const client = createClient();
 
 /**
  * Итак, у нас есть очередь ожидания соперника
@@ -16,61 +14,77 @@ const USERS_GAME_PAIRS_MAP_KEY = 'game_pairs_map';
 const MS_WAIT_IN_QUEUE = 60 * 1000;
 
 const timersToDelete = new Map<number, Timeout>();
+//
+// export const test = async () => {
+//   await client.hset('eee', 'eee', '222');
+//   console.log(await client.hget('eee', 'eee'));
+// }
 
 export class RedisService {
-  public static async redisMakeGamePair(user1: number, user2: number) {
-    await client.hset(USERS_GAME_PAIRS_MAP_KEY, user1.toString(), user2.toString());
-    await client.hset(USERS_GAME_PAIRS_MAP_KEY, user2.toString(), user1.toString());
+  private client: RedisClient;
+
+  constructor() {
+    this.client = createClient();
   }
 
-  public static async redisEndGame(userId: number) {
-    const pairedUser = await client.hget(USERS_GAME_PAIRS_MAP_KEY, userId.toString());
-    await client.hdel(USERS_GAME_PAIRS_MAP_KEY, userId.toString());
+  public async test() {
+    await this.client.hset('eee', 'eee', '222');
+    console.log(await this.client.hget('eee', 'eee'));
+  }
+
+  public async redisMakeGamePair(user1: number, user2: number) {
+    await this.client.hset(USERS_GAME_PAIRS_MAP_KEY, user1.toString(), user2.toString());
+    await this.client.hset(USERS_GAME_PAIRS_MAP_KEY, user2.toString(), user1.toString());
+  }
+
+  public async redisEndGame(userId: number) {
+    const pairedUser = await this.client.hget(USERS_GAME_PAIRS_MAP_KEY, userId.toString());
+    await this.client.hdel(USERS_GAME_PAIRS_MAP_KEY, userId.toString());
     if (pairedUser) {
-      await client.hdel(USERS_GAME_PAIRS_MAP_KEY, pairedUser);
+      await this.client.hdel(USERS_GAME_PAIRS_MAP_KEY, pairedUser);
     }
   };
 
-  public static async redisGetPair(userId: number): Promise<string | null> {
-    return client.hget(
+  public async redisGetPair(userId: number): Promise<string | null> {
+    return this.client.hget(
       USERS_GAME_PAIRS_MAP_KEY,
       userId.toString()
     );
   };
 
-  public static async redisSetInQueue(userId: number): Promise<boolean> {
+  public async redisSetInQueue(userId: number): Promise<boolean> {
     if (await this.redisIsUserWaiting(userId)) {
       console.log(`Игрок ${userId} уже в очереди, размер очереди: ${await this.redisGetQueueSize()}`);
       return false;
     }
-    await client.rpush(USERS_QUEUE_KEY, userId.toString());
-    await client.hset(USERS_MAP_KEY, userId.toString(), (+new Date()).toString());
+    await this.client.rpush(USERS_QUEUE_KEY, userId.toString());
+    await this.client.hset(USERS_MAP_KEY, userId.toString(), (+new Date()).toString());
     timersToDelete.set(+userId, setTimeout(async () => {
-      await client.hdel(USERS_MAP_KEY, userId.toString());
+      await this.client.hdel(USERS_MAP_KEY, userId.toString());
       console.log('deleted', userId);
     }, MS_WAIT_IN_QUEUE));
     console.log(`Игрок ${userId} встал в ожидание, размер очереди: ${await this.redisGetQueueSize()}`);
     return true;
   };
 
-  public static async redisGetQueueSize(): Promise<number> {
-    return client.llen(USERS_QUEUE_KEY);
+  public async redisGetQueueSize(): Promise<number> {
+    return this.client.llen(USERS_QUEUE_KEY);
   }
 
-  public static async redisPopUser(): Promise<string | null> {
+  public async redisPopUser(): Promise<string | null> {
     let valid = false;
     let userId: string | null = null;
     while (!valid) {
-      userId = await client.lpop(USERS_QUEUE_KEY);
+      userId = await this.client.lpop(USERS_QUEUE_KEY);
       if (userId !== null) {
-        const time = await client.hget(USERS_MAP_KEY, userId);
+        const time = await this.client.hget(USERS_MAP_KEY, userId);
         if (!time || +new Date() - +time > MS_WAIT_IN_QUEUE) {
           // почему-то не удалили, мб перезапуск - берём дальше
           valid = false;
         } else {
           valid = true;
         }
-        await client.hdel(USERS_MAP_KEY, userId);
+        await this.client.hdel(USERS_MAP_KEY, userId);
         if (timersToDelete.has(+userId)) {
           clearTimeout(+(timersToDelete.get(+userId) || 0));
           timersToDelete.delete(+userId);
@@ -86,11 +100,11 @@ export class RedisService {
   /**
    * Находится ли юзер в очереди
    */
-  public static async redisIsUserWaiting(userId: number): Promise<{
+  public async redisIsUserWaiting(userId: number): Promise<{
     start: number;
     now: number;
   } | null> {
-    const time = await client.hget(USERS_MAP_KEY, userId.toString());
+    const time = await this.client.hget(USERS_MAP_KEY, userId.toString());
     if (time !== null && +new Date() - +time <= MS_WAIT_IN_QUEUE) {
       return {
         start: +time,
---
+++ src/tests/redis.test.ts
@@ -0,0 +1,9 @@
+// import {RedisService} from "../../lib";
+//
+// const redis = new RedisService();
+//
+// (async () => {
+//   console.log(await redis.redisSetInQueue(222));
+//   console.log(await redis.redisIsUserWaiting(222));
+// })();
+//
