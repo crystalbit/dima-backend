0020cd20fb02bce85c33303da83f245c8f9080fb
Dmitrii Korotkin <d.korotkin@corp.vk.com>
2021-03-19 06:48:59 +0300

    queues

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { RedisService } from './services/redis.service';
-import { WebsocketService } from "./services/websocket.service";
+import { WebsocketService } from './services/websocket.service';
+import { QueueService } from './services/queue.service';
 
 export const redis = new RedisService();
 export const sockets = new WebsocketService();
+export const queues = new QueueService(redis, sockets);
---
+++ src/services/queue.service.ts
@@ -0,0 +1,14 @@
+import { RedisService } from "./redis.service";
+import { WebsocketService } from "./websocket.service";
+
+export class QueueService {
+  public constructor(
+    private readonly redis: RedisService,
+    private readonly ws: WebsocketService,
+  ) { }
+
+  public async uqMakePair(user1: number, user2: number) {
+    await this.redis.redisMakeGamePair(user1, user2);
+    await this.ws.sendWerePaired(user1, user2);
+  };
+}
